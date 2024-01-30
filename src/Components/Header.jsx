7e693f08911f175a0242541560fa5c251256be18
SardorFargodOgli <sardoribnfarhod@gmail.com>
2024-01-31 00:39:41 +0500

    profile menu

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,8 +2,41 @@ import React, { useState } from "react";
 import Logout from "../Pages/Signout";
 import { FaUser } from "react-icons/fa";
 import "../styles/header.scss";
+import { FiEdit } from "react-icons/fi";
+import { MdOutlineAddBox } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
+import { GoSignOut } from "react-icons/go";
 
 function Header({ userInfo }) {
+  const [showProfileMenu, setShowProfileMenu] = useState(false);
+
+  const handleShowProfileMenu = () => {
+    setShowProfileMenu(!showProfileMenu);
+  };
+
+  // Logout
+  const handleLogout = async () => {
+    try {
+      const token = localStorage.getItem("authToken");
+      const response = await fetch("http://127.0.0.1:8000/api/v1/logout/", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.status === 200) {
+        console.log("Logout successful");
+        localStorage.removeItem("authToken");
+        window.location.href = "/signin";
+      } else {
+        console.log("Logout failed");
+      }
+    } catch (error) {
+      console.error("Error during logout:", error);
+    }
+  };
+
   return (
     <header className="header">
       <div className="container">
@@ -17,9 +50,29 @@ function Header({ userInfo }) {
           </div>
           <div className="profile-info">
             <div className="username">{userInfo.username}</div>
-            <FaUser className="user-img" />
+            <FaUser onClick={handleShowProfileMenu} className="user-img" />
+            {showProfileMenu && (
+              <ul className="profile-menu">
+                <li className="menu-item">
+                  <FiEdit />
+                  <span>Edit</span>
+                </li>
+                <li className="menu-item">
+                  <MdOutlineAddBox />
+                  <span>Add</span>
+                </li>
+                <hr />
+                <li className="menu-item">
+                  <CgProfile />
+                  <span>Profile</span>
+                </li>
+                <li className="menu-item" onClick={handleLogout}>
+                  <GoSignOut />
+                  <span>Logout</span>
+                </li>
+              </ul>
+            )}
           </div>
-          <Logout />
         </div>
       </div>
     </header>
