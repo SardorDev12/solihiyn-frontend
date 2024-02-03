a029933463362b9d08256f2a49f0e2ecf8f877e2
SardorFargodOgli <sardoribnfarhod@gmail.com>
2024-02-04 00:40:53 +0500

    minor fix

--- src/Components/Context.jsx
+++ src/Components/Context.jsx
@@ -3,8 +3,8 @@ import { createContext, useContext } from "react";
 const APIContext = createContext();
 
 const ContextProvider = ({ children }) => {
-  const api = "http://127.0.0.1:8000/";
-  // const api = "https://sardorfarhodogli.pythonanywhere.com/";
+  // const api = "http://127.0.0.1:8000/";
+  const api = "https://sardorfarhodogli.pythonanywhere.com/";
 
   return <APIContext.Provider value={api}>{children}</APIContext.Provider>;
 };
