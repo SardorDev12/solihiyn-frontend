b2645731b23651df6b760ea08db9b6ae33b18e8f
SardorFargodOgli <sardoribnfarhod@gmail.com>
2024-02-13 23:10:26 +0500

    fix how to use

--- src/Pages/HowToUse.jsx
+++ src/Pages/HowToUse.jsx
@@ -5,7 +5,6 @@ import view3 from "../Images/view 3.png";
 import view4 from "../Images/view 4.png";
 import view5 from "../Images/view 5.png";
 import view6 from "../Images/view 6.png";
-import view7 from "../Images/view 7.png";
 
 function HowToUse() {
   return (
@@ -40,9 +39,7 @@ function HowToUse() {
         </div>
         <div className="case">
           <h3 className="case-title">
-            4. Zikrs you add will show in main page like this. On the header of
-            each card, thers are edit and delete icons so that you can add and
-            delete them.
+            4. Zikrs you add will show in main page like this.
           </h3>
           <div className="case-img">
             <img src={view4} alt="view" />
@@ -50,8 +47,8 @@ function HowToUse() {
         </div>
         <div className="case">
           <h3 className="case-title">
-            5. When you click the edit icon, this modal will show. You can
-            update the fields you want and submit.
+            5. On the header of each card, there are edit, reset and delete
+            icons so that you can update, reset and delete items.
           </h3>
           <div className="case-img">
             <img src={view5} alt="view" />
@@ -59,19 +56,11 @@ function HowToUse() {
         </div>
         <div className="case">
           <h3 className="case-title">
-            6. When you click the meaning link, meaning modal will show.
-          </h3>
-          <div className="case-img">
-            <img src={view6} alt="view" />
-          </div>
-        </div>
-        <div className="case">
-          <h3 className="case-title">
-            7. if you want to leave the system, you can click the logout button
+            6. If you want to leave the system, you can click the logout button
             on the profile menu.
           </h3>
           <div className="case-img">
-            <img src={view7} alt="view" />
+            <img src={view6} alt="view" />
           </div>
         </div>
       </div>
